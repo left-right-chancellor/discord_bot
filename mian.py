7de5591be8ee5f95925c0faef04e8cf140aa1dfa
louischo303 <louischo303@gmail.com>
2025-10-08 13:01:32 +0800

    Update bot

--- mian.py
+++ mian.py
@@ -242,14 +242,13 @@ async def on_error(interaction: discord.Interaction, error: Exception):
 
 
 try:
-  token = os.getenv("TOKEN") or ""
-  if token == "":
-    raise Exception("Please add your token to the Secrets pane.")
+  token = os.getenv('TOKEN') or ''
+  if token == '':
+    raise Exception('Please add your token to the Secrets pane.')
   keep_alive()
   bot.run(token)
 except discord.HTTPException as e:
     if e.status == 429:
-        print('The Discord servers denied the connection for making too many requests')
-        print('Get help from https://stackoverflow.com/questions/66724687/in-discord-py-how-to-solve-the-error-for-toomanyrequests')
+        print('The Discord servers denied the connection for making too many requests\nGet help from https://stackoverflow.com/questions/66724687/in-discord-py-how-to-solve-the-error-for-toomanyrequests')
     else:
         raise e
