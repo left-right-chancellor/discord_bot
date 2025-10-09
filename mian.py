23d5f9f73a3c0a3705a35421656b32b34cd193a1
louischo303 <louischo303@gmail.com>
2025-10-09 08:40:23 +0800

    Update bot

--- mian.py
+++ mian.py
@@ -13,7 +13,7 @@ WIDE_MAP = {i: i + 0xFEE0 for i in range(0x21, 0x7F)}
 WIDE_MAP[0x20] = 0x3000
 
 def is_made_up_of(m, s):
-    if not m:
+    if not m or len(m) != len(s):
         return False
     i = 0
     m = m.upper()
