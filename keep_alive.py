6404159673a86ef0bf7dce790a8d51dd6d781624
louischo303 <louischo303@gmail.com>
2025-10-10 08:38:40 +0800

    Update bot

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def main():
-    return '<!DOCTYPE html><html><head><meta charset="utf-8" /><title>男娘機機</title><style>body{background:#202020;color:#fff;}</style></head><body>Operating. </body></html>'
+    return '<!DOCTYPE html><html><head><meta charset="utf-8" /><title>男娘機機</title><style>body{background:#202020;color:#fff;font-family:monospace;}</style></head><body>機機> python3 mian.py<br />Operating. </body></html>'
 
 def run():
     app.run(host="0.0.0.0", port=8080)
